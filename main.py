a0ff3436b1ae3c6417821794b5809eb011a539f2
Dillon Barnes <83948303+DillonB07@users.noreply.github.com>
2021-11-11 16:35:53 +0000

    Update main.py

--- main.py
+++ main.py
@@ -121,8 +121,9 @@ def avatar():
 def page_not_found(e):
   return render_template('404.html')
 
-"""@app.errorhandler(500)
+@app.errorhandler(500)
 def page_not_found2(e):
-  return render_template("nouser.html") # The most likely outcome is because the user doesn't exist, so we're assuming that because of that, it will always be a no user error. We could be wrong though!"""
+  return render_template("505.html") 
+# The most likely outcome is because the user doesn't exist, so we're assuming that because of that, it will always be a no user error. We could be wrong though!
   
 app.run(host="0.0.0.0", port=8080)
